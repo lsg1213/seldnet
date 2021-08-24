8ddd349b44e9a5f251849c32bb37c887dfcddca4
Seungjin Lee <lsg0385@naver.com>
2021-08-24 13:54:03 +0000

    [add] test search_space in multi gpu environment

--- test_searchspace.py
+++ test_searchspace.py
@@ -12,6 +12,7 @@ import models
 
 train_config = args.parse_args()
 os.environ['CUDA_VISIBLE_DEVICES'] = train_config.gpus
+mirrored_strategy = tf.distribute.MirroredStrategy()
 train_config.n_repeat = 1
 
 block_2d_num = max(block_2d_num)
@@ -29,24 +30,38 @@ search_space['SED'] = {'search_space_1d': search_space_1d}
 search_space['DOA'] = {'search_space_1d': search_space_1d}
 
 model_configs = get_max_configs(train_config, search_space, [300,64,7], postprocess_fn)
-trainset = get_dataset(train_config, mode='train')
+if train_config.multi:
+    with mirrored_strategy.scope():
+        valset = get_dataset(train_config, mode='train')
+else:
+    valset = get_dataset(train_config, mode='train')
 
 for model_config in model_configs:
     optimizer = tf.keras.optimizers.Adam(train_config.lr)
-    model = models.conv_temporal([300,64,7], model_config)
-    model.summary()
+    if train_config.multi:
+        with mirrored_strategy.scope():
+            model = models.conv_temporal([300,64,7], model_config)
+            model.summary()
 
-    model.compile(optimizer=optimizer,
-                  loss={'sed_out': tf.keras.losses.BinaryCrossentropy(),
-                        'doa_out': tf.keras.losses.MSE},
-                  loss_weights=[1, 1000])
+            model.compile(optimizer=optimizer,
+                        loss={'sed_out': tf.keras.losses.BinaryCrossentropy(),
+                                'doa_out': tf.keras.losses.MSE},
+                        loss_weights=[1, 1000])
+    else:
+        model = models.conv_temporal([300,64,7], model_config)
+        model.summary()
+
+        model.compile(optimizer=optimizer,
+                    loss={'sed_out': tf.keras.losses.BinaryCrossentropy(),
+                            'doa_out': tf.keras.losses.MSE},
+                    loss_weights=[1, 1000])
     for k,v in model_config.items():
         print(k)
         print(v)
         print('------------------------------------')
     with open('too_big_search.json', 'w') as f:
         json.dump(model_config, f, indent=4)
-    model.fit(trainset)
+    model.fit(valset)
     os.system('rm -rf too_big_search.json')
 print('All search space is available in this GPU')
 
