6f317d3f2bef2b55593a86d456e8b8a93113c2ca
Seungjin Lee <lsg0385@naver.com>
2021-08-29 09:41:17 +0000

    [add] amending code for key of search_space

--- result_error_fix.py
+++ result_error_fix.py
@@ -1,14 +1,16 @@
 import json
+from search import search_space_1d, search_space_2d
 
 
-with open('result/initial/result_3.json','r') as f:
+with open('result/initial_score/result_1.json','r') as f:
     results = json.load(f)
 
 for result in results:
     blocks = [i for i in result['config'].keys() if i.startswith('BLOCK') and not 'ARGS' in i]
+    keys = ['n_classes', 'BLOCK0', 'BLOCK0_ARGS', 'BLOCK1', 'BLOCK1_ARGS', 'BLOCK2', 'BLOCK2_ARGS', 'BLOCK3', 'BLOCK3_ARGS', 'SED', 'SED_ARGS', 'DOA', 'DOA_ARGS']
+
     if len(blocks) != 4:
         tmp = {}
-        keys = ['n_classes', 'BLOCK0', 'BLOCK0_ARGS', 'BLOCK1', 'BLOCK1_ARGS', 'BLOCK2', 'BLOCK2_ARGS', 'BLOCK3', 'BLOCK3_ARGS', 'SED', 'SED_ARGS', 'DOA', 'DOA_ARGS']
         i = 0
         while i < len(keys):
             if keys[i].startswith('BLOCK'):
@@ -33,5 +35,28 @@ for result in results:
                 tmp[keys[i]] = result['config'][keys[i]]
                 i += 1
         result['config'] = tmp
-with open('result/initial/result_3.json','w') as f:
+    
+    for block in ['BLOCK0', 'BLOCK1', 'BLOCK2', 'BLOCK3', 'SED', 'DOA']:
+        args = result['config'].get(block)
+        if args is None:
+            raise ValueError('something wrong')
+        search_space_1d.update(search_space_2d)
+        sp = search_space_1d[args]
+        for k in [i for i in result['config'][block + '_ARGS'].keys()]:
+            v = result['config'][block + '_ARGS'].get(k)
+            tmp = {}
+            if not k in sp.keys():
+                if args != 'simple_dense_stage':
+                    newkey = '_'.join([args.split('_')[0],k])
+                else:
+                    newkey = 'dense_' + k
+                for key in result['config'][block + '_ARGS'].keys(): # 순서 맞추려고 이렇게 함
+                    if k != key:
+                        tmp[key] = result['config'][block + '_ARGS'][key]
+                    else:
+                        tmp[newkey] = result['config'][block + '_ARGS'][key]
+
+                result['config'][block + '_ARGS'] = tmp
+                
+with open('result/result_1.json','w') as f:
     json.dump(results, f, indent=4)
