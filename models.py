6e799966965287fc43686ac6e52a5b0d6c835b54
Seungjin Lee <lsg0385@naver.com>
2021-09-16 01:06:53 +0000

    [add] accdoa model

--- models.py
+++ models.py
@@ -78,6 +78,31 @@ def conv_temporal(input_shape, model_config):
     return tf.keras.Model(inputs=inputs, outputs=[sed, doa])
 
 
+def accdoa(input_shape, model_config):
+    filters = model_config.get('filters', 32)
+    first_kernel_size = model_config.get('first_kernel_size', 7)
+    first_pool_size = model_config.get('first_pool_size', [5, 1])
+    n_classes = model_config.get('n_classes', 14)
+
+    inputs = Input(shape=input_shape[-3:])
+    
+    x = layers.conv2d_bn(filters, first_kernel_size, padding='same', 
+                         activation='relu')(inputs)
+    x = MaxPooling2D(first_pool_size, padding='same')(x)
+
+    blocks = [key for key in model_config.keys()
+              if key.startswith('BLOCK') and not key.endswith('_ARGS')]
+    blocks.sort()
+
+    for block in blocks:
+        x = getattr(modules, model_config[block])(model_config[f'{block}_ARGS'])(x)
+
+    doa = getattr(modules, model_config['DOA'])(model_config['DOA_ARGS'])(x)
+    doa = Dense(3*n_classes, activation='tanh', name='doa_out')(doa)
+
+    return tf.keras.Model(inputs=inputs, outputs=doa)
+
+
 def vad_architecture(input_shape, model_config):
     flatten = model_config.get('flatten', True)
     last_unit = model_config.get('last_unit', 1)
